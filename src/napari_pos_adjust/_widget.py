63eae3410bd7df9e0ef513f80738fd67bb2c1fb1
Tiancheng Liu <tianchengliu.com@gmail.com>
2023-01-23 08:11:02 -0600

    test layer

--- src/napari_pos_adjust/_widget.py
+++ src/napari_pos_adjust/_widget.py
@@ -24,6 +24,12 @@ class Widget(QWidget):
     affine_matrix = np.array(
         [[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]]
     )
+    translate_x = 0
+    translate_y = 0
+    translate_z = 0
+    rotation_x = 0
+    rotation_y = 0
+    rotation_z = 0
 
     def __init__(self, napari_viewer):
         super().__init__()
@@ -36,22 +42,47 @@ class Widget(QWidget):
         self.sl_translate_x.setMinimum(-5000)
         self.sl_translate_x.setMaximum(5000)
         self.sl_translate_x.setValue(0)
-        self.sl_translate_x.valueChanged.connect(self.x_value_changed)
+        self.sl_translate_x.valueChanged.connect(
+            self.translate_x_value_changed
+        )
         self.sl_translate_y = QSlider(Qt.Horizontal)
         self.sl_translate_y.setMinimum(-5000)
         self.sl_translate_y.setMaximum(5000)
         self.sl_translate_y.setValue(0)
-        self.sl_translate_y.valueChanged.connect(self.y_value_changed)
+        self.sl_translate_y.valueChanged.connect(
+            self.translate_y_value_changed
+        )
         self.sl_translate_z = QSlider(Qt.Horizontal)
         self.sl_translate_z.setMinimum(-2500)
         self.sl_translate_z.setMaximum(2500)
         self.sl_translate_z.setValue(0)
-        self.sl_translate_z.valueChanged.connect(self.z_value_changed)
+        self.sl_translate_z.valueChanged.connect(
+            self.translate_z_value_changed
+        )
+        # Rotation sliders
+        self.sl_rotate_x = QSlider(Qt.Horizontal)
+        self.sl_rotate_x.setMinimum(-180)
+        self.sl_rotate_x.setMaximum(180)
+        self.sl_rotate_x.setValue(0)
+        self.sl_rotate_x.valueChanged.connect(self.rotate_x_value_changed)
+        self.sl_rotate_y = QSlider(Qt.Horizontal)
+        self.sl_rotate_y.setMinimum(-180)
+        self.sl_rotate_y.setMaximum(180)
+        self.sl_rotate_y.setValue(0)
+        self.sl_rotate_y.valueChanged.connect(self.rotate_y_value_changed)
+        self.sl_rotate_z = QSlider(Qt.Horizontal)
+        self.sl_rotate_z.setMinimum(-180)
+        self.sl_rotate_z.setMaximum(180)
+        self.sl_rotate_z.setValue(0)
+        self.sl_rotate_z.valueChanged.connect(self.rotate_z_value_changed)
 
         layout = QFormLayout()
         layout.addRow("translate x:", self.sl_translate_x)
         layout.addRow("translate y:", self.sl_translate_y)
         layout.addRow("translate z:", self.sl_translate_z)
+        layout.addRow("rotate x:", self.sl_rotate_x)
+        layout.addRow("rotate y:", self.sl_rotate_y)
+        layout.addRow("rotate z:", self.sl_rotate_z)
         self.setLayout(layout)
         # self.layout().addWidget(self.btn)
         # self.layout().addWidget(self.sl_translate_x)
@@ -66,20 +97,30 @@ class Widget(QWidget):
     #     translate_val[2] = 1000
     #     self.viewer.layers["0"].translate = translate_val
 
-    def x_value_changed(self):
+    def translate_x_value_changed(self):
         self.affine_matrix[2][3] = self.sl_translate_x.value()
-        # self.viewer.layers["0"].translate =
-        # [0, 0, self.sl_translate_x.value()]
         self.viewer.layers["0"].affine = self.affine_matrix
+        print(self.viewer.layers["0"].metadata)
 
-    def y_value_changed(self):
+    def translate_y_value_changed(self):
         self.affine_matrix[1][3] = self.sl_translate_y.value()
-        # self.viewer.layers["0"].translate =
-        # [0, 0, self.sl_translate_x.value()]
         self.viewer.layers["0"].affine = self.affine_matrix
 
-    def z_value_changed(self):
+    def translate_z_value_changed(self):
         self.affine_matrix[0][3] = self.sl_translate_z.value()
-        # self.viewer.layers["0"].translate =
-        # [0, 0, self.sl_translate_x.value()]
         self.viewer.layers["0"].affine = self.affine_matrix
+
+    def rotate_x_value_changed(self):
+        # TODO
+        return
+
+    def rotate_y_value_changed(self):
+        # TODO
+        return
+
+    def rotate_z_value_changed(self):
+        # TODO
+        return
+
+    def calculate_affine(self):
+        return
