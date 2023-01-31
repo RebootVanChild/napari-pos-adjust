93d2f731cfa8c7ed9f46ed83c9b2a1468b983c4a
Tiancheng Liu <tianchengliu.com@gmail.com>
2023-01-30 18:22:14 -0600

    magic factory

--- src/napari_pos_adjust/_widget.py
+++ src/napari_pos_adjust/_widget.py
@@ -443,7 +443,7 @@ def GetAffineMatrixFromLandmarks(
         "min": -5000,
         "max": 5000,
     },
-    btn={"widget_type": "PushButton", "name": "button"},
+    btn={"widget_type": "PushButton", "value": False, "name": "button"},
 )
 def example_magic_widget(
     viewer: Viewer,
